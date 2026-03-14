85509c4fcff5a38d0696531e9289cf4b7e290745
Amarjeet Kumar Patel <147983593+amarjeetpatel@users.noreply.github.com>
2026-03-14 22:10:06 +0530

    Time: 4 ms (98%), Space: 54.7 MB (94.39%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+
+    let count = new Array(26).fill(0);
+
+    for (let i = 0; i < s.length; i++) {
+        count[s.charCodeAt(i) - 97]++;
+        count[t.charCodeAt(i) - 97]--;
+    }
+
+    for (let i = 0; i < 26; i++) {
+        if (count[i] !== 0) return false;
+    }
+
+    return true;
+};
