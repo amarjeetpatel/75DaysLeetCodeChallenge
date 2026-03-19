1bdd7ad459af7c812b797e02681e20d469318837
Amarjeet Kumar Patel <147983593+amarjeetpatel@users.noreply.github.com>
2026-03-19 18:08:32 +0530

    Time: 4 ms (76.69%), Space: 57.7 MB (66.68%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        while (left < right && !isAlphaNum(s[left])) {
+            left++;
+        }
+
+        while (left < right && !isAlphaNum(s[right])) {
+            right--;
+        }
+
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+};
+
+function isAlphaNum(char) {
+    return (
+        (char >= 'a' && char <= 'z') ||
+        (char >= 'A' && char <= 'Z') ||
+        (char >= '0' && char <= '9')
+    );
+}
