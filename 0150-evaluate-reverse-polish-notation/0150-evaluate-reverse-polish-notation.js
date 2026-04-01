e025ec08a836b8f7f70a5d23f87e9488c1454a11
Amarjeet Kumar Patel <147983593+amarjeetpatel@users.noreply.github.com>
2026-04-01 13:38:20 +0530

    Time: 7 ms (44.77%), Space: 56.4 MB (86.51%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    const stack = [];
+
+    for (let token of tokens) {
+        if (token === '+' || token === '-' || token === '*' || token === '/') {
+            const b = stack.pop();
+            const a = stack.pop();
+
+            let result;
+
+            if (token === '+') result = a + b;
+            else if (token === '-') result = a - b;
+            else if (token === '*') result = a * b;
+            else {
+                // Truncate toward zero
+                result = (a / b) | 0;
+            }
+
+            stack.push(result);
+        } else {
+            stack.push(Number(token));
+        }
+    }
+
+    return stack[0];
+};
