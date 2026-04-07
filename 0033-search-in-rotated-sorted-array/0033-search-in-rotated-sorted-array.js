2985ff409106d467ce05a711c1ec2fdf3d736576
Amarjeet Kumar Patel <147983593+amarjeetpatel@users.noreply.github.com>
2026-04-07 15:01:34 +0530

    Time: 0 ms (100%), Space: 53.3 MB (88.34%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+function search(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = left + ((right - left) >> 1);
+
+        if (nums[mid] === target) return mid;
+
+        // Left half is sorted
+        if (nums[left] <= nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } 
+        // Right half is sorted
+        else {
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
